2379b78b9ded6b760ecc8ca8ee7a74f2411a1cc9
Safa-80 <safa.vettiyattil.com>
2026-09-02 21:54:32 +0530

    Add format specifiers practice file

---
+++ format_specifiers.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int myNum = 45;
+    printf("%d\n", myNum);
+    float myFloatNum = 9.88;
+    printf("%f\n", myFloatNum);
+    char myLetter = 'H';
+    printf("%c\n", myLetter);
+    return 0;
+}
